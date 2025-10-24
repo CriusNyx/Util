28c217b397d5d478f795105e957cb6248bacb141
CriusNyx <rjreynoldsw@gmail.com>
2025-10-24 16:36:45 -0400

    Added As and WhereAs

---
+++ core/src/LinqExtensions.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+
+namespace CriusNyx.Util;
+
+public static class LinqExtensions
+{
+  public static T? As<T>(this object value)
+  {
+    if (value is T t)
+    {
+      return t;
+    }
+    return default;
+  }
+
+  public static IEnumerable<T> WhereAs<T>(this IEnumerable enumerable)
+  {
+    foreach (var element in enumerable)
+    {
+      if (element is T t)
+      {
+        yield return t;
+      }
+    }
+  }
+}
---
+++ tests/src/LinqExtensions.Tests.cs
@@ -0,0 +1,39 @@
+using System.Runtime.InteropServices;
+using CriusNyx.Util;
+
+class ClassA { }
+
+class ClassB { }
+
+public class LinqExtensionsTests
+{
+  [Test]
+  public void As_Returns_When_Correct()
+  {
+    var a = new ClassA();
+    object o = a;
+    var actual = o.As<ClassA>();
+
+    Assert.That(actual, Is.EqualTo(a));
+  }
+
+  [Test]
+  public void As_Returns_Null_When_Not_Correct()
+  {
+    var a = new ClassB();
+    object o = a;
+    var actual = o.As<ClassA>();
+
+    Assert.Null(actual);
+  }
+
+  [Test]
+  public void WhereAs_Filters_Array()
+  {
+    var a = new ClassA();
+    var b = new ClassB();
+    object[] arr = [a, b];
+    var actual = arr.WhereAs<ClassA>();
+    Assert.That(actual, Is.EquivalentTo(new ClassA[] { a }));
+  }
+}
